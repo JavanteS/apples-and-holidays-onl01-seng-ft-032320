3b0f5cde9954e925241c09b5cc1c4166a75ec806
Javante Nelson <javantenelson@gmail.com>
2020-03-29 03:40:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,13 +55,16 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, day| 
-  day.each do | fun, decor| 
-      if decor.include?("BBQ")
-   return fun
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, items|
+      if items.include?("BBQ")
+        holiday
       end
     end
-  end
+  end.flatten.compact
 end
 
 
